73ae8b49a0ed3fb86a08d5ef0b4f0321c66af5df
mleibeng <mleibeng@student.42.fr>
2025-02-04 04:02:14 +0100

    Added initial third project

---
+++ ChefClaude/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+            <img src="https://www.shutterstock.com/discover/free-nature-images"/>
+            <p>Hello World</p>
+        </>
+    )
+}
