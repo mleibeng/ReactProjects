609519a9c7a75a22f7be041313715592a9456886
mleibeng <mleibeng@student.42heilbronn.de>
2025-02-06 05:57:17 +0100

    Added submit logic

--- ChefClaude/src/App.jsx
+++ ChefClaude/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Main from "./components/Main"
 
 export default function App() {
+
   return (
     <>
       <Header/>
--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,10 +1,30 @@
+import React from "react";
+
 export default function Main() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map( ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const formdata = new FormData(event.currentTarget)
+        const newIngredient = formdata.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+        event.currentTarget.reset()
+    }
+
     return (
         <main>
-            <form className="add-ingredients-form">
-                <input type="text" aria-label="Add ingredient" placeholder="e.g. pepper"/>
-                <button type="text">Add ingredient</button>
+            <form className="add-ingredients-form" onSubmit={handleSubmit}>
+                <input type="text" aria-label="Add ingredient" placeholder="e.g. pepper" name="ingredient" />
+                <button type="submit" >Add ingredient</button>
             </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
     )
 }
